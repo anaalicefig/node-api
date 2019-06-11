9f459c0e693bae7dc366f62940bc20cc22679be6
Ana Alice <anaalicefm18@gmail.com>
2019-06-11 18:37:19 -0300

    corrigindo erro do cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cores = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
